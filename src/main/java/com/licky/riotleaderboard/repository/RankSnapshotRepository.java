50284037d2a3ed79d66963d36802789d50dbb91b
sukteMung <tur58332@temple.edu>
2026-09-17 20:03:45 -0400

    Fixed RankSnapshopRepository

--- src/main/java/com/licky/riotleaderboard/repository/RankSnapshotRepository.java
+++ src/main/java/com/licky/riotleaderboard/repository/RankSnapshotRepository.java
@@ -3,5 +3,5 @@ package com.licky.riotleaderboard.repository;
 import com.licky.riotleaderboard.model.RankSnapshot;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RankRepository extends JpaRepository<RankSnapshot, Long> {
+public interface RankSnapshotRepository extends JpaRepository<RankSnapshot, Long> {
 }
