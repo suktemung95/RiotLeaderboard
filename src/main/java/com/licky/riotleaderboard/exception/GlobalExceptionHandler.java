3e91b3872c7b93122cb4e78a6ba15d77a072949d
sukteMung <tur58332@temple.edu>
2026-09-18 11:26:07 -0400

    Added GlobalException handling and handled PlayerNotFound exception

---
+++ src/main/java/com/licky/riotleaderboard/exception/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.licky.riotleaderboard.exception;
+
+import com.licky.riotleaderboard.dto.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler extends RuntimeException {
+
+    @ExceptionHandler(PlayerNotFoundException.class)
+    public ResponseEntity<ApiResponse<Void>> handlePlayerNotFound(
+            PlayerNotFoundException ex
+    ) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(
+                        new ApiResponse<>(
+                                "error",
+                                ex.getMessage(),
+                                null
+                        )
+                );
+    }
+
+
+}
--- src/main/java/com/licky/riotleaderboard/exception/PlayerNotFoundException.java
+++ src/main/java/com/licky/riotleaderboard/exception/PlayerNotFoundException.java
@@ -2,6 +2,6 @@ package com.licky.riotleaderboard.exception;
 
 public class PlayerNotFoundException extends RuntimeException {
     public PlayerNotFoundException(Long id) {
-        super("PLayer not found with id: " + id);
+        super("Player not found with id: " + id);
     }
 }
